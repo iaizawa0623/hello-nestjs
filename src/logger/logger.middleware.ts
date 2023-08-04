35436f781a14b0a601ed3731f29ace61937d935c
Ibuki Aizawa <aizawa_ibuki@odawarakiki.com>
2023-08-04 14:20:26 +0900

    chore: added comment to LoggerMiddleware

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -1,10 +1,13 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 
+/**
+ * Recording access logs
+ */
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    const datetime = new Date();
+    const datetime = new Date().toISOString();
     const clientIpAddress = req.ip;
     const headers = req.headers;
     const url = req.originalUrl;
