1339e47568f53fe1e6b7803251708df1641b38ce
Ibuki Aizawa <aizawa_ibuki@odawarakiki.com>
2023-08-07 16:36:39 +0900

    add inspectors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './validation/validation.pipe';
+import { LoggingInterceptor } from './logging/logging.interceptor';
+import { TransformInterceptor } from './transform/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(
+    new LoggingInterceptor(),
+    new TransformInterceptor(),
+  );
   await app.listen(3000);
 }
 bootstrap();
