ae62d2edf0e10c6a41c73fef8474d38869fb1fd4
Ibuki Aizawa <aizawa_ibuki@odawarakiki.com>
2023-08-08 11:42:10 +0900

    feat: implement nest/swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from './validation/validation.pipe';
 import { LoggingInterceptor } from './logging/logging.interceptor';
 import { TransformInterceptor } from './transform/transform.interceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,11 @@ async function bootstrap() {
     new LoggingInterceptor(),
     new TransformInterceptor(),
   );
+
+  const config = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
